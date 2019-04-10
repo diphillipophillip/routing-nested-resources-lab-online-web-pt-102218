ace4c661dce490f894ca78759435793b5594ff2e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-04-10 01:28:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
     if @song.nil?
       redirect_to artist_songs_path(@artist), alert: "Song not found"
     else 
-      @song = Song.find_by(params[:id])
+      @song = Song.find(params[:id])
     end 
   end
 
