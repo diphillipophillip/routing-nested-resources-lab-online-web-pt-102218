642ccbf0661a5e5c22967ac8702dbaf14046b5fa
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-04-10 01:23:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,18 @@
 class SongsController < ApplicationController
   
-
+ def index
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
+    else
+      @songs = Song.all
+    end
+  end
+  
   def show
     @song = Song.find(params[:id])
   end
